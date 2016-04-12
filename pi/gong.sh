34e05463ec4d9ef9bf83c8c3d292809014c667f7
Ben Olive <ben.olive@salesloft.com>
2016-04-12 10:40:49 -0400

    Recalibrate gong.sh based on actual setup

--- pi/gong.sh
+++ pi/gong.sh
@@ -6,10 +6,15 @@ gpio pwm-ms             # Set PWM mode to mark:space
 gpio pwmc 1920          # Set the clock divisor to 1920
 gpio pwmr 200           # Set the range to 200
 
-gpio pwm $PWM_PIN 25    # Raise hammer
+gpio pwm $PWM_PIN 15    # Raise hammer
 sleep 0.6               # Wait for servo to reach position
-gpio pwm $PWM_PIN 6     # Swing hammer
-sleep 0.6               # Wait for servo to reach position
-gpio pwm $PWM_PIN 11    # Move hammer off the gong to allow resonation
-sleep 1                 # Hold for a moment before disabling
-gpio mode $PWM_PIN in   # Set pin mode to input
+gpio pwm $PWM_PIN 8     # Swing hammer
+sleep 0.2               # Wait for servo to reach position
+gpio pwm $PWM_PIN 15    # Move hammer off the gong to allow resonation
+sleep 2                 # Hold for a moment before disabling
+
+# Slowly return to neutral position
+for p in `seq 14 -1 10`; do
+  gpio pwm $PWM_PIN $p
+  sleep 0.1
+done
